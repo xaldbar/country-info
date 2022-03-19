3be276057513dac910af79adfffe3938adc49120
Alex Baranov <xaldbar@gmail.com>
2022-03-19 23:30:41 +0300

    add loader

--- src/features/countries/CountryList.jsx
+++ src/features/countries/CountryList.jsx
@@ -1,54 +1,55 @@
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
-import { List } from '../../components/List';
-import { Card } from '../../components/Card';
-import { useCountries } from './use-countries';
+import {List} from '../../components/List';
+import {Card} from '../../components/Card';
+import {useCountries} from './use-countries';
+import Loader from '../../UI/Loader/Loader';
 
 
 const CountryList = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [countries, {error, status}] = useCountries();
+    const [countries, {error, status}] = useCountries();
 
-  return (
-    <>
-      {error && <h2>Can't fetch data</h2>}
-      {status === 'loading' && <h2>Loading...</h2>}
+    return (
+        <>
+            {error && <h2>Can't fetch data</h2>}
+            {status === 'loading' && <Loader/>}
 
-      {status === 'received' && (
-      <List>
-        {countries.map((c) => {
-          const countryInfo = {
-            img: c.flags.png,
-            name: c.name,
-            info: [
-              {
-                title: 'Population',
-                description: c.population.toLocaleString(),
-              },
-              {
-                title: 'Region',
-                description: c.region,
-              },
-              {
-                title: 'Capital',
-                description: c.capital,
-              },
-            ],
-          };
+            {status === 'received' && (
+                <List>
+                    {countries.map((c) => {
+                        const countryInfo = {
+                            img: c.flags.png,
+                            name: c.name,
+                            info: [
+                                {
+                                    title: 'Population',
+                                    description: c.population.toLocaleString(),
+                                },
+                                {
+                                    title: 'Region',
+                                    description: c.region,
+                                },
+                                {
+                                    title: 'Capital',
+                                    description: c.capital,
+                                },
+                            ],
+                        };
 
-          return (
-            <Card
-              key={c.name}
-              onClick={() => navigate(`/country/${c.name}`)}
-              {...countryInfo}
-            />
-          );
-        })}
-      </List>
-      )}
-    </>
-  )
+                        return (
+                            <Card
+                                key={c.name}
+                                onClick={() => navigate(`/country/${c.name}`)}
+                                {...countryInfo}
+                            />
+                        );
+                    })}
+                </List>
+            )}
+        </>
+    )
 }
 
 export {CountryList};
--- src/features/details/CountryDetails.jsx
+++ src/features/details/CountryDetails.jsx
@@ -1,13 +1,13 @@
 import { Info } from './Info';
 import { useDetails } from './use-details';
-
+import Loader from '../../UI/Loader/Loader'
 
 const CountryDetails = ({name = '', navigate}) => {
   const {status, error, currentCountry} = useDetails(name);
 
   return (
     <>
-      {status === 'loading' && <h2>Loading...</h2>}
+      {status === 'loading' && <Loader/>}
       {error && <h2>{error}</h2>}
       {currentCountry && <Info push={navigate} {...currentCountry} />}
     </>
